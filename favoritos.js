3daf5ff0fa3aef316a86e136542e0af852e88e45
Marcos de Oliveira Brandão Junior <marcosde.o.b.junior@gmail.com>
2025-05-10 10:07:26 -0300

    Create favoritos.js

---
+++ favoritos.js
@@ -0,0 +1,17 @@
+window.favoritos = [
+  {
+    title: "Alice no País das Maravilhas",
+    image: "https://m.media-amazon.com/images/I/71oOakHPmVL._AC_UF1000,1000_QL80_.jpg",
+    link: "https://www.notion.so/Alice-no-Pa-s-das-Maravilhas-1e8af0392b16801b97d1d44d38fb98bd?pvs=4"
+  },
+  {
+    title: "Estudo em Vermelho",
+    image: "https://m.media-amazon.com/images/I/81tmn7qB-bL.jpg",
+    link: "https://www.notion.so/Estudo-em-Vermelho-1e8af0392b168058a144f7f5f82f9e02?pvs=4"
+  },
+  {
+    title: "Harry Potter e a Pedra Filosofal",
+    image: "https://m.media-amazon.com/images/I/81ibfYk4qmL.jpg",
+    link: "https://www.notion.so/Harry-Potter-e-a-Pedra-Filosofal-1e8af0392b168009bb3dce7ed3f6ec6c?pvs=4"
+  }
+];
